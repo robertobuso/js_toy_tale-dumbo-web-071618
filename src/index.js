6889c7922658fbf989f06717d526ca08708c4011
github username <github email address>
2018-09-05 10:29:24 -0400

    doing Step 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,39 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
-// YOUR CODE HERE
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:3000/toys')
+  .then(r => r.json())
+  .then(toys => makeToy(toys))
+  }
+)
+
+function makeToy(toys){
+
+  toys.forEach(toy => {
+    const collectionDiv = document.getElementById('toy-collection')
+    const toyDiv = document.createElement('div')
+    toyDiv.classList.add('card')
+    toyDiv.dataset.cardID = toy.id
+    const toyName = document.createElement('h2')
+    const toyLikes = document.createElement('p')
+    const toyPic = document.createElement('img')
+    toyPic.classList.add('toy-avatar')
+
+    toyName.innerHTML = toy.name
+    toyLikes.innerHTML = toy.likes
+    toyPic.src = toy.image
+
+    toyDiv.append(toyName)
+    toyDiv.append(toyPic)
+    toyDiv.append(toyLikes)
+
+    collectionDiv.append(toyDiv)
+
+  })
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
